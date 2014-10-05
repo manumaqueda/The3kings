8047a0a92d22af213430fe93bcbe16bbf48b75ad
Vanily <manumaqueda@gmail.com>
2014-10-05 11:28:38 +0200

    geen

--- src/com/example/holamundo/Mapa_Parques.java
+++ src/com/example/holamundo/Mapa_Parques.java
@@ -61,7 +61,7 @@ public class Mapa_Parques extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
+		// Handle action bar item clicks here. The action bar will	
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
