84f125d0a1473b230d0b9ae0c3aabc4eb04ff781
EduPonz <e.ponzs@gmail.com>
2014-10-21 20:36:50 +0200

    Deja de bacilar

--- src/com/Parques_Nacionales/Tabs_Activity.java
+++ src/com/Parques_Nacionales/Tabs_Activity.java
@@ -11,7 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Tabs_Activity extends ActionBarActivity {
-    //Manu Guapo 
+    //Manu bobo 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
