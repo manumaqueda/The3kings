e41eae45f11075d79499b0d5bf5fb1fd708aeb87
EduPonz <e.ponzs@gmail.com>
2014-11-02 11:58:04 +0100

    Probando para juntar todas las actividades de las pestañas

--- src/com/Parques_Nacionales/FragmentInfo.java
+++ src/com/Parques_Nacionales/FragmentInfo.java
@@ -1,5 +1,7 @@
 package com.Parques_Nacionales;
 
+import com.Parques_Nacionales.Tabs_Activity.TabsListener;
+
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,12 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentInfo extends Fragment {
-
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	       
-		View tabcontent=inflater.inflate(R.layout.fragment_info, container, false);
-		return tabcontent;
 		
-	    }
+		View tabcontent=inflater.inflate(R.layout.fragment_info, container, false); 
+		
+		switch (TabsListener.pos){
+			case '0':
+				tabcontent=inflater.inflate(R.layout.fragment_info, container, false);
+			case '1':
+				tabcontent=inflater.inflate(R.layout.fragment_fauna, container, false);
+			case '2':
+				tabcontent=inflater.inflate(R.layout.fragment_flora, container, false);
+			case '3':
+				tabcontent=inflater.inflate(R.layout.fragment_otros, container, false);
+		} 
+		return tabcontent;
+	}
 	
 }
